01b263cf89221198b6e5af333cf7c1389c49f60d
YouseF <usefz89@gmail.com>
2025-09-21 08:14:03 +0300

    Initial Commit

---
+++ ForsaApp/ForsaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForsaAppApp.swift
+//  ForsaApp
+//
+//  Created by Yousef Zuriqi on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ForsaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
